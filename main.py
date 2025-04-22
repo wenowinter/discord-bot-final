074727602706428e8848b8e23558eb39d5e23b66
wenowinter <136591975+wenowinter@users.noreply.github.com>
2025-04-22 23:36:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -620,12 +620,6 @@ async def pomoc(ctx):
 # Zabezpieczenie tokena
 TOKEN = os.environ.get('DISCORD_TOKEN')
 if not TOKEN:
-    TOKEN = '
-# Zabezpieczenie tokena
-TOKEN = os.environ.get('DISCORD_TOKEN')
-if not TOKEN:
     TOKEN = 'MTM2Mzk1OTQ3NjU5OTY1MjQ3Mg.GOOBo0.L8uMzfNPX89QAJcMFw-XIMZPOSo_iandoZR-fk'  # Zastąp swoim tokenem, jeśli nie używasz zmiennych środowiskowych
-
-bot.run(TOKEN)'  # Zastąp swoim tokenem, jeśli nie używasz zmiennych środowiskowych
 
 bot.run(TOKEN)
